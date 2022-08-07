ffd8dbba1f7aa6ebfa936858882f7ffb6dd8c676
Maro 43 <mariusz.olbrys@outlook.com>
2022-08-07 21:57:32 +0200

    AbbreviateTwoWordName My first solution

--- src/main/java/olbrys/mariusz/learn/eightkyu/AbbreviateTwoWordName.java
+++ src/main/java/olbrys/mariusz/learn/eightkyu/AbbreviateTwoWordName.java
@@ -12,6 +12,22 @@ patrick feeney => P.F
 public class AbbreviateTwoWordName {
 
     public static String abbrevName(String name) {
-        return name;
+        String[] split = name.split(" ");
+        String result = "";
+        for (String s : split) {
+            char charAt = s.charAt(0);
+            System.out.println(charAt);
+        }
+        System.out.println("Mariusz Olbryś");
+        System.out.println(split[0].charAt(0) + "." + split[1].charAt(0));
+
+
+        return name + "." + name;
     }
+
+    public static void main(String[] args) {
+        abbrevName("Mariusz Olbryś");
+        abbrevName("Sam Harris");
+    }
+
 }
