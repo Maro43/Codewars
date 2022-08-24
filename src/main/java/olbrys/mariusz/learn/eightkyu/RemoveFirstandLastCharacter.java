03dcc6939e079581a57e75534e9ee62547943556
Maro 43 <mariusz.olbrys@outlook.com>
2022-08-24 23:08:49 +0200

    RemoveFirstandLastCharacter Solution

--- src/main/java/olbrys/mariusz/learn/eightkyu/RemoveFirstandLastCharacter.java
+++ src/main/java/olbrys/mariusz/learn/eightkyu/RemoveFirstandLastCharacter.java
@@ -2,8 +2,7 @@ package olbrys.mariusz.learn.eightkyu;
 
 public class RemoveFirstandLastCharacter {
     public static String removeChars(String str) {
-        str = str.substring(1, str.length() - 1);
-        return str;
+        return str.substring(1, str.length() - 1);
     }
 
     public static void main(String[] args) {
